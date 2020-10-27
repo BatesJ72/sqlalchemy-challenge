5630fb53dc5bcf122b3af64a888ede213b6d866a
Jessica Bates <bates.jessica72@gmail.com>
2020-10-27 23:32:58 +0000

    progress

--- queries.py
+++ queries.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from pprint import pprint
 import pandas as pd
+from sqlalchemy import func, select, table, column
 
 
 # Import Hawaii Measurement Data
@@ -36,21 +37,26 @@ station_data = station_session.query(Hawaii_Station)
 # Date of trip: 7/1/2016 - 7/9/2016
 conn_m = measurement_engine.connect()
 
+## Should I drop all NULLs? May matter for the joined table
+measurement_df1 = pd.read_sql("SELECT * FROM measurement", conn_m).dropna(how = 'any') 
+# print(measurement_df1)
+# print(measurement_df1.head())
+
 # Get max date from table
 max_df = pd.read_sql("SELECT max(date) FROM measurement", conn_m)
 # print(max_df)
 
 # Get pertinent data
-measurement_df1 = pd.read_sql("SELECT date, prcp FROM measurement WHERE date between '2015-07-01' and '2016-07-01'", conn_m)
+measurement_df2 = pd.read_sql("SELECT date, prcp FROM measurement WHERE date between '2015-07-01' and '2016-07-01'", conn_m)
 # print(measurement_df1)
 # print(measurement_df1.head())
 
 # Data Cleaning: set index, sort, drop NULLs
-measurement_df2 = measurement_df1.sort_values(by = 'date').dropna(how = 'any') #set_index('date').
-# print(measurement_df2)
+measurement_df3 = measurement_df2.sort_values(by = 'date').dropna(how = 'any') #set_index('date').
+# print(measurement_df3)
 
 # Plot data
-measurement_plot = measurement_df2.plot(x = 'date', y = 'prcp', kind = 'bar')
+measurement_plot = measurement_df3.plot(x = 'date', y = 'prcp', kind = 'bar')
 # measurement_plot.show
 
 # Summary Statistics
@@ -73,5 +79,11 @@ station_count = station_df1["station"].count()
 # The most active stations
 
 # Join the data together
-joined_df = measurement_df2.join(station_df1, on = "station", how = "left")
-print(joined_df.head())
+all_df = pd.merge(measurement_df1, station_df1, on = "station", how = "left")
+# print(all_df.head())
+
+# List of stations and observation counts in descending order
+print(all_df.count())
+
+
+
